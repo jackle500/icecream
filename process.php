c70fe15b9138f8e26f53396a2c2c8b8ea0beb2fa
Huy Le <jackle50098@gmail.com>
2024-01-09 16:13:54 -0500

    utilize the code

--- process.php
+++ process.php
@@ -1,27 +1,31 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Ice Cream Order summary</title>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ice Cream Order Summary</title>
 </head>
 <body>
 <h1>Thank you for your order!</h1>
+
 <?php
 
     //Turn on error reporting
-    ini_set('display_errors');
+    ini_set('display_errors', 1);
     error_reporting(E_ALL);
 
     //Define constants
-    define("PRICE_PER_SCOOP", 2.50);
-    define('SALE_TAX_RATE', 0.11);
+    define('PRICE_PER_SCOOP', 2.50);
+    define('SALES_TAX_RATE', 0.11);
 
-   //For testing purposes only
-        /*
-        echo "<pre>";
-        var_dump($_POST);
-        echo "</pre>";
-        */
+    //For testing purposes only
+    /*
+    echo "<pre>";
+    var_dump($_POST);
+    echo "</pre>";
+    */
 
     //Get data from POST array
     if (!empty($_POST['scoops'])){
@@ -51,7 +55,7 @@
 
     //Calculate total due
     $subtotal = PRICE_PER_SCOOP * $scoops;
-    $tax = $subtotal * SALE_TAX_RATE;
+    $tax = $subtotal * SALES_TAX_RATE;
     $total = $subtotal + $tax;
 
     //Print a summary
@@ -62,10 +66,7 @@
     echo "<p>Tax: $tax</p>";
     echo "<p>Total: $$total</p>";
 
-
-
-
-
 ?>
+
 </body>
 </html>
