eabc307b1fc7b0d62350566bbee1e1859d20dfe6
Huy Le <jackle50098@gmail.com>
2024-01-09 13:26:23 -0500

    Update the process page to print form POST

--- process.php
+++ process.php
@@ -6,5 +6,20 @@
 </head>
 <body>
 <h1>Thank you for your order!</h1>
+<?php
+var_dump($_POST);
+echo "</pre";
+
+
+//get data form POST array
+$scoops = $_POST('scoops');
+$flavors = $_POST('flavors');
+$flavorsString = implode(", ", $flavors);
+
+//print a summary
+    echo "<p>$scoops Scoops</p>";
+    echo "<p>Flavors: $flavorsString</p>"
+
+?>
 </body>
 </html>
